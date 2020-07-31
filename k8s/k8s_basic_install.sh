ccbfd86567173fcd6219fe95664cb9526ba528d8
mabt <mbt@wegloo.org>
2020-07-31 22:16:50 +0200

    new commit

--- k8s/k8s_basic_install.sh
+++ k8s/k8s_basic_install.sh
@@ -6,29 +6,26 @@ mount -a
 curl -fsSL https://get.docker.com | sh;
 sudo usermod -aG docker $USER
 
-groupadd -g 500000 dockremap && 
-groupadd -g 501000 dockremap-user && 
-useradd -u 500000 -g dockremap -s /bin/false dockremap && 
-useradd -u 501000 -g dockremap-user -s /bin/false dockremap-user
+# groupadd -g 500000 dockremap && 
+# groupadd -g 501000 dockremap-user && 
+# useradd -u 500000 -g dockremap -s /bin/false dockremap && 
+# useradd -u 501000 -g dockremap-user -s /bin/false dockremap-user
 
-echo "dockremap:500000:65536" >> /etc/subuid && 
-echo "dockremap:500000:65536" >>/etc/subgid
+# echo "dockremap:500000:65536" >> /etc/subuid && 
+# echo "dockremap:500000:65536" >>/etc/subgid
 
-echo "
-  {
-   \"userns-remap\": \"default\"
-  }
-" > /etc/docker/daemon.json
+# echo "
+#   {
+#    \"userns-remap\": \"default\"
+#   }
+# " > /etc/docker/daemon.json
 
 systemctl daemon-reload && systemctl restart docker
 
 # k8s install
-
-apt-get update && apt-get install -y apt-transport-https curl gnupg
+apt-get update && apt-get install -y apt-transport-https curl gnupg software-properties-common
 curl -s https://packages.cloud.google.com/apt/doc/apt-key.gpg | apt-key add -
-cat <<EOF >/etc/apt/sources.list.d/kubernetes.list
-deb https://apt.kubernetes.io/ kubernetes-xenial main
-EOF
+add-apt-repository "deb http://apt.kubernetes.io/ kubernetes-xenial main"
 apt-get update
 apt-get install -y kubelet kubeadm kubectl kubernetes-cni
 systemctl enable kubelet
