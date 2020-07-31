6bf35c28902fdd3ede7d5da727dac8a894f563d2
mabt <mbt@wegloo.org>
2020-07-31 21:22:03 +0200

    new commit

--- k8s/k8s_basic_install.sh
+++ k8s/k8s_basic_install.sh
@@ -2,6 +2,28 @@ swapoff -a
 sed -e '/.*none.*swap.*/ s/^#*/#/' -i /etc/fstab
 mount -a
 
+# docker install
+curl -fsSL https://get.docker.com | sh;
+sudo usermod -aG docker $USER
+
+groupadd -g 500000 dockremap && 
+groupadd -g 501000 dockremap-user && 
+useradd -u 500000 -g dockremap -s /bin/false dockremap && 
+useradd -u 501000 -g dockremap-user -s /bin/false dockremap-user
+
+echo "dockremap:500000:65536" >> /etc/subuid && 
+echo "dockremap:500000:65536" >>/etc/subgid
+
+echo "
+  {
+   \"userns-remap\": \"default\"
+  }
+" > /etc/docker/daemon.json
+
+systemctl daemon-reload && systemctl restart docker
+
+# k8s install
+
 apt-get update && apt-get install -y apt-transport-https curl gnupg
 curl -s https://packages.cloud.google.com/apt/doc/apt-key.gpg | apt-key add -
 cat <<EOF >/etc/apt/sources.list.d/kubernetes.list
