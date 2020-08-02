0258a2aeb6655f94b0d95708e0a4ac919483a32f
mabt <mbt@wegloo.org>
2020-08-02 13:53:43 +0200

    new commit

--- k8s/k8s_basic_install.sh
+++ k8s/k8s_basic_install.sh
@@ -2,10 +2,10 @@ swapoff -a
 sed -e '/.*none.*swap.*/ s/^#*/#/' -i /etc/fstab
 mount -a
 
-apt-get update && apt-get install -y apt-transport-https ca-certificates curl software-properties-common gnupg2
+apt-get update && apt-get install -y apt-transport-https ca-certificates curl software-properties-common gnupg2 > /dev/null 2>&1
 
 # docker
-curl -fsSL https://get.docker.com | sh;
+curl -fsSL https://get.docker.com | sh; > /dev/null 2>&1
 sudo usermod -aG docker $USER
 groupadd -g 500000 dockremap && 
 groupadd -g 501000 dockremap-user && 
@@ -21,13 +21,13 @@ echo "
   }
 " > /etc/docker/daemon.json
 
-systemctl daemon-reload && systemctl restart docker
+systemctl daemon-reload && systemctl restart docker > /dev/null 2>&1
 
 
 # k8s install
 curl -s https://packages.cloud.google.com/apt/doc/apt-key.gpg | apt-key add -
 add-apt-repository "deb http://apt.kubernetes.io/ kubernetes-xenial main"
-apt-get update
-apt-get install -y kubelet kubeadm kubectl kubernetes-cni
+apt-get update > /dev/null 2>&1
+apt-get install -y kubelet kubeadm kubectl kubernetes-cni > /dev/null 2>&1
 systemctl enable kubelet
 apt-mark hold kubelet kubeadm kubectl kubernetes-cni
