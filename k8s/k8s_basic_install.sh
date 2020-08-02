303e7f409c97875ec7b90a3ab00940c487fc15a4
mabt <mbt@wegloo.org>
2020-08-02 15:44:07 +0200

    new commit

--- k8s/k8s_basic_install.sh
+++ k8s/k8s_basic_install.sh
@@ -1,33 +1,31 @@
 #!/bin/bash
-# Run the commands in a noninteractive mode
-export DEBIAN_FRONTEND=noninteractive
-# Update source list and log the output to file
-apt-get update && apt-get -y install apt-transport-https ca-certificates curl software-properties-common gnupg2 > /home/install.log
 
 # swapoff -a 
+apt-get update && apt-get -y install apt-transport-https ca-certificates curl software-properties-common gnupg2 > /home/install.log
 
-# # docker install
-# curl -fsSL https://get.docker.com | sh &&
-# usermod -aG docker $USER &&
-# groupadd -g 500000 dockremap && 
-# groupadd -g 501000 dockremap-user && 
-# useradd -u 500000 -g dockremap -s /bin/false dockremap && 
-# useradd -u 501000 -g dockremap-user -s /bin/false dockremap-user
+# docker install
+curl -fsSL https://get.docker.com | sh &&
+usermod -aG docker $USER
 
-# echo "dockremap:500000:65536" >> /etc/subuid && 
-# echo "dockremap:500000:65536" >> /etc/subgid &&
-# echo "
-#   {
-#    \"userns-remap\": \"default\"
-#   }
-# " > /etc/docker/daemon.json
+groupadd -g 500000 dockremap && 
+groupadd -g 501000 dockremap-user && 
+useradd -u 500000 -g dockremap -s /bin/false dockremap && 
+useradd -u 501000 -g dockremap-user -s /bin/false dockremap-user
 
-# systemctl daemon-reload &&
-# systemctl restart docker
+echo "dockremap:500000:65536" >> /etc/subuid && 
+echo "dockremap:500000:65536" >>/etc/subgid
 
-# # k8s install
-# curl -s https://packages.cloud.google.com/apt/doc/apt-key.gpg | apt-key add -
-# add-apt-repository "deb http://apt.kubernetes.io/ kubernetes-xenial main"
-# apt-get update && apt-get install -y kubelet kubeadm kubectl kubernetes-cni
-# systemctl enable kubelet
-# apt-mark hold kubelet kubeadm kubectl kubernetes-cni
+echo "
+  {
+   \"userns-remap\": \"default\"
+  }
+" > /etc/docker/daemon.json
+
+systemctl daemon-reload && systemctl restart docker
+
+# k8s install
+curl -s https://packages.cloud.google.com/apt/doc/apt-key.gpg | apt-key add -
+add-apt-repository "deb http://apt.kubernetes.io/ kubernetes-xenial main"
+apt-get update && apt-get install -y kubelet kubeadm kubectl kubernetes-cni
+systemctl enable kubelet
+apt-mark hold kubelet kubeadm kubectl kubernetes-cni
