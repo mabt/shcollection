9cbfa4a2b27f3d331f7d2d28987b0ba1e4f60383
mabt <mbt@wegloo.org>
2020-08-14 20:25:50 +0200

    new commit

---
+++ grafana/grafana_basic_install.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+apt-get install -y apt-transport-https
+apt-get install -y software-properties-common wget
+wget -q -O - https://packages.grafana.com/gpg.key | apt-key add -
+
+echo "deb https://packages.grafana.com/oss/deb stable main" | tee -a /etc/apt/sources.list.d/grafana.list
+
+apt-get update && apt-get install grafana -y
