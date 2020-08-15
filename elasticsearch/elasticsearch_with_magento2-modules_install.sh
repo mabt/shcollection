aa908632d8f2859661546d23fc497b3b1000de50
mabt <mbt@wegloo.org>
2020-08-15 14:48:42 +0200

    new commit

---
+++ elasticsearch/elasticsearch_with_magento2-modules_install.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+wget -qO - https://artifacts.elastic.co/GPG-KEY-elasticsearch | sudo apt-key add -
+
+[ ! -f /etc/apt/sources.list.d/elastic-7.x.list] && echo "deb https://artifacts.elastic.co/packages/7.x/apt stable main" | sudo -a /etc/apt/sources.list.d/elastic-7.x.list
+
+sudo apt-get update
+sudo apt-get install -y apt-transport-https
+
+apt-get update && apt-get -y install elasticsearch
+
+systemctl enable elasticsearsh
+systemctl daemon-reload
+systemctl start elasticsearsh
+
+/usr/share/elasticsearch/bin/elasticsearch-plugin -s install analysis-phonetic
+/usr/share/elasticsearch/bin/elasticsearch-plugin -s install analysis-icu
--- influxdata/influxdata_basic_install.sh
+++ influxdata/influxdata_basic_install.sh
@@ -2,7 +2,7 @@
 
 wget -qO- https://repos.influxdata.com/influxdb.key | sudo apt-key add -
 source /etc/os-release
-echo "deb https://repos.influxdata.com/debian $(lsb_release -cs) stable" | tee /etc/apt/sources.list.d/influxdb.list
+[ ! -f /etc/apt/sources.list.d/influxdb.list ] && echo "deb https://repos.influxdata.com/debian $(lsb_release -cs) stable" | tee /etc/apt/sources.list.d/influxdb.list
 
 apt-get update && apt-get install -y influxdb
 systemctl enable influxdb
