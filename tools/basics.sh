239fa66d8ba410a71c07c5124a247f12d24c065c
mabt <mbt@wegloo.org>
2020-08-24 11:42:35 +0200

    new commit

---
+++ tools/basics.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+apt-get update && apt-get -y install apt-transport-https ca-certificates curl software-properties-common gnupg2 bash-completion lynx net-tools emacs-nox tmux ncdu zip dnsutils
