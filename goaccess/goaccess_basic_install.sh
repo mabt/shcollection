e7df9e318859d3f33e28e715dc372328698737c4
mabt <mbt@wegloo.org>
2020-10-28 11:13:29 +0100

    new commit

---
+++ goaccess/goaccess_basic_install.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+cd /root/
+wget https://tar.goaccess.io/goaccess-1.4.tar.gz
+tar -xzvf goaccess-1.4.tar.gz
+cd goaccess-1.4/
+./configure --enable-utf8 --enable-geoip=legacy
+make
+make install
