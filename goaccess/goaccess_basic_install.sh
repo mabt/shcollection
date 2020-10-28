b25ff5016add2a8d7ab8680e6febb2652fb68dbd
mabt <mbt@wegloo.org>
2020-10-28 11:51:00 +0100

    new commit

--- goaccess/goaccess_basic_install.sh
+++ goaccess/goaccess_basic_install.sh
@@ -8,3 +8,7 @@ cd goaccess-1.4/
 ./configure --enable-utf8 --enable-geoip=legacy
 make
 make install
+
+sed -i '/time-format %H:%M:%S/s/^#//g' /usr/local/etc/goaccess/goaccess.conf
+sed -i '/log-format %h %^\[%d:%t %^\] "%r" %s %b "%R" "%u"/s/^#//g' /usr/local/etc/goaccess/goaccess.con
+sed -i '/date-format %d\/%b\/%Y/s/^#//g' /usr/local/etc/goaccess/goaccess.conf
