854b5351b1c55d85e785007659c4421c66a80524
mabt <mbt@wegloo.org>
2020-11-05 18:17:49 +0100

    new commit

--- varnish/varnish_basic_install.sh
+++ varnish/varnish_basic_install.sh
@@ -2,10 +2,11 @@
 
 apt update && apt-get install -y varnish
 
-curl https://raw.githubusercontent.com/mabt/shcollection/master/varnish/varnishncsa-m2dev.service --output /lib/systemd/system/varnishncsa-m2dev.service
-curl https://raw.githubusercontent.com/mabt/shcollection/master/varnish/varnish-m2dev.service --output /lib/systemd/system/varnish-m2dev.service
-curl https://raw.githubusercontent.com/mabt/shcollection/master/varnish/default.m2dev.vcl  --output /etc/varnish/default.m2dev.vcl
+#curl https://raw.githubusercontent.com/mabt/shcollection/master/varnish/varnishncsa-m2dev.service --output /lib/systemd/system/varnishncsa-m2dev.service
+#curl https://raw.githubusercontent.com/mabt/shcollection/master/varnish/varnish-m2dev.service --output /lib/systemd/system/varnish-m2dev.service
+#curl https://raw.githubusercontent.com/mabt/shcollection/master/varnish/default.m2dev.vcl  --output /etc/varnish/default.m2dev.vcl
+#curl https://raw.githubusercontent.com/mabt/shcollection/master/varnish/apache2-vhost-varnished-exemple.conf --output /etc/apache2/sites-available/apache2-vhost-varnished-exemple.con
 
-systemctl enable varnish-m2dev
-systemctl daemon-reload
-systemctl restart varnish-m2dev
+#systemctl enable varnish-m2dev
+#systemctl daemon-reload
+#systemctl restart varnish-m2dev
