5bdd64a0018c282942ede7694b63b857f0e83312
mabt <mbt@wegloo.org>
2020-11-05 16:40:51 +0100

    new commit

--- varnish/varnish_basic_install.sh
+++ varnish/varnish_basic_install.sh
@@ -4,9 +4,8 @@ apt update && apt-get install -y varnish
 
 curl https://raw.githubusercontent.com/mabt/shcollection/master/varnish/varnishncsa-m2dev.service --output /lib/systemd/system/varnishncsa-m2dev.service
 curl https://raw.githubusercontent.com/mabt/shcollection/master/varnish/varnish-m2dev.service --output /lib/systemd/system/varnish-m2dev.service
+curl https://raw.githubusercontent.com/mabt/shcollection/master/varnish/default.m2dev.vcl  --output /etc/varnish/default.m2dev.vcl
 
-
-#systemctl enable varnish
-
-
-
+systemctl enable varnish-m2dev
+systemctl daemon-reload
+systemctl restart varnish-m2dev
