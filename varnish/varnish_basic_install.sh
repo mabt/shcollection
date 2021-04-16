9e635f7fe3b1dbba7865f9844ab65b2e088cabc2
mabt <mbt@wegloo.org>
2021-04-16 15:43:45 +0200

    update

--- varnish/varnish_basic_install.sh
+++ varnish/varnish_basic_install.sh
@@ -1,23 +1,24 @@
-#!/bin/bash
+#!/bin/bash                                                                                                                                                                                                                                                                            
 
 NAME="prod-m2"
-WEBSITE="ServerName example.com"
-PORT="6092"
-PATH="///"
-
-apt update && apt-get install -y varnish
+PORT="10081"
+PORT2="10082"
+apt-get update && apt-get install -y curl varnish                                                                                                                                                                                                                                    
 
 curl https://raw.githubusercontent.com/mabt/shcollection/master/varnish/varnishncsa-666.service --output /lib/systemd/system/varnishncsa-$NAME.service
 curl https://raw.githubusercontent.com/mabt/shcollection/master/varnish/varnish-666.service --output /lib/systemd/system/varnish-$NAME.service
 curl https://raw.githubusercontent.com/mabt/shcollection/master/varnish/default.666.vcl  --output /etc/varnish/default.$NAME.vcl
-curl https://raw.githubusercontent.com/mabt/shcollection/master/varnish/apache2-vhost-varnished-exemple.conf --output /etc/apache2/sites-available/apache2-vhost-varnished-exemple.con
+curl https://raw.githubusercontent.com/mabt/shcollection/master/varnish/apache2-vhost-varnished-exemple.conf --output /etc/apache2/varnish-example.conf
 
-sed -i 's/666/$NAME/g' /lib/systemd/system/varnishncsa-$NAME.service
-sed -i 's/666/$NAME/g' /lib/systemd/system/varnish-$NAME.service
-sed -i 's/ServerName example.com/$WEBSITE/g' /etc/apache2/sites-available/apache2-vhost-varnished-exemple.con
-sed -i 's/6091/$PORT/g' /etc/apache2/sites-available/apache2-vhost-varnished-exemple.con
-sed -i 's/\/home\/user\/www\/example.com/$PATH/g' /etc/apache2/sites-available/apache2-vhost-varnished-exemple.con
+sed -i 's/666/'${NAME}'/g' /lib/systemd/system/varnishncsa-$NAME.service
+sed -i 's/666/'${NAME}'/g' /lib/systemd/system/varnish-$NAME.service
+sed -i 's/66666/'${PORT}'/g' /lib/systemd/system/varnish-$NAME.service
+sed -i 's/77777/'${PORT}'/g' /lib/systemd/system/varnish-$NAME.service
+
+mkdir /var/lib/varnish/$NAME
+chown varnish. /var/lib/varnish/$NAME
 
 systemctl enable varnish-$NAME
 systemctl daemon-reload
 systemctl restart varnish-$NAME
+
