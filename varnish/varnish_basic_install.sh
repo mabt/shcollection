d22e43b04f5002f58853d443dd53d9fcb69b6dcb
mabt <mbt@wegloo.org>
2021-04-16 15:22:19 +0200

    update

--- varnish/varnish_basic_install.sh
+++ varnish/varnish_basic_install.sh
@@ -1,12 +1,23 @@
 #!/bin/bash
 
+NAME="prod-m2"
+WEBSITE="ServerName example.com"
+PORT="6092"
+PATH="///"
+
 apt update && apt-get install -y varnish
 
-#curl https://raw.githubusercontent.com/mabt/shcollection/master/varnish/varnishncsa-m2dev.service --output /lib/systemd/system/varnishncsa-m2dev.service
-#curl https://raw.githubusercontent.com/mabt/shcollection/master/varnish/varnish-m2dev.service --output /lib/systemd/system/varnish-m2dev.service
-#curl https://raw.githubusercontent.com/mabt/shcollection/master/varnish/default.m2dev.vcl  --output /etc/varnish/default.m2dev.vcl
-#curl https://raw.githubusercontent.com/mabt/shcollection/master/varnish/apache2-vhost-varnished-exemple.conf --output /etc/apache2/sites-available/apache2-vhost-varnished-exemple.con
+curl https://raw.githubusercontent.com/mabt/shcollection/master/varnish/varnishncsa-666.service --output /lib/systemd/system/varnishncsa-$NAME.service
+curl https://raw.githubusercontent.com/mabt/shcollection/master/varnish/varnish-666.service --output /lib/systemd/system/varnish-$NAME.service
+curl https://raw.githubusercontent.com/mabt/shcollection/master/varnish/default.666.vcl  --output /etc/varnish/default.$NAME.vcl
+curl https://raw.githubusercontent.com/mabt/shcollection/master/varnish/apache2-vhost-varnished-exemple.conf --output /etc/apache2/sites-available/apache2-vhost-varnished-exemple.con
 
-#systemctl enable varnish-m2dev
-#systemctl daemon-reload
-#systemctl restart varnish-m2dev
+sed -i 's/666/$NAME/g' /lib/systemd/system/varnishncsa-$NAME.service
+sed -i 's/666/$NAME/g' /lib/systemd/system/varnish-$NAME.service
+sed -i 's/ServerName example.com/$WEBSITE/g' /etc/apache2/sites-available/apache2-vhost-varnished-exemple.con
+sed -i 's/6091/$PORT/g' /etc/apache2/sites-available/apache2-vhost-varnished-exemple.con
+sed -i 's/\/home\/user\/www\/example.com/$PATH/g' /etc/apache2/sites-available/apache2-vhost-varnished-exemple.con
+
+systemctl enable varnish-$NAME
+systemctl daemon-reload
+systemctl restart varnish-$NAME
