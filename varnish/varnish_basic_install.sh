1b03b22e1a7489a62b09b133ea66acf2214d8647
mabt <mbt@wegloo.org>
2021-04-16 16:50:18 +0200

    update

--- varnish/varnish_basic_install.sh
+++ varnish/varnish_basic_install.sh
@@ -6,10 +6,13 @@ PORT2="10082"
 
 apt-get update && apt-get install -y curl varnish                                                                                                                                                                                                                                    
 
+a2enmod remoteip
+
 curl https://raw.githubusercontent.com/mabt/shcollection/master/varnish/varnishncsa-666.service --output /lib/systemd/system/varnishncsa-$NAME.service
 curl https://raw.githubusercontent.com/mabt/shcollection/master/varnish/varnish-666.service --output /lib/systemd/system/varnish-$NAME.service
 curl https://raw.githubusercontent.com/mabt/shcollection/master/varnish/default.666.vcl  --output /etc/varnish/default.$NAME.vcl
 curl https://raw.githubusercontent.com/mabt/shcollection/master/varnish/apache2-vhost-varnished-exemple.conf --output /etc/apache2/varnish-example.conf
+curl https://raw.githubusercontent.com/mabt/shcollection/master/varnish/apache2-varnish.conf --output /etc/apache2/conf-enabled/varnish.conf
 
 sed -i 's/666/'${NAME}'/g' /lib/systemd/system/varnishncsa-$NAME.service
 sed -i 's/666/'${NAME}'/g' /lib/systemd/system/varnish-$NAME.service
@@ -19,12 +22,13 @@ sed -i 's/77777/'${PORT}'/g' /lib/systemd/system/varnish-$NAME.service
 [[ -e /var/lib/varnish/${NAME} ]] || mkdir /var/lib/varnish/$NAME
 chown varnish. /var/lib/varnish/$NAME
 
+systemctl daemon-reload
 systemctl enable varnish-$NAME
 systemctl daemon-reload
 systemctl restart varnish-$NAME
 
 echo ""
-echo "systemctl status varnish-'${NAME}'.service"
+echo "systemctl status varnish-${NAME}.service"
 echo ""
 echo "Port :"
 echo $PORT
