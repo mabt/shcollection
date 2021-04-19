70b0ef910df6ca813f7c938c646348368850abcc
mabt <mbt@wegloo.org>
2021-04-19 09:51:39 +0200

    update

--- varnish/varnish_basic_install.sh
+++ varnish/varnish_basic_install.sh
@@ -11,13 +11,13 @@ a2enmod remoteip
 curl https://raw.githubusercontent.com/mabt/shcollection/master/varnish/varnishncsa-666.service --output /lib/systemd/system/varnishncsa-$NAME.service
 curl https://raw.githubusercontent.com/mabt/shcollection/master/varnish/varnish-666.service --output /lib/systemd/system/varnish-$NAME.service
 curl https://raw.githubusercontent.com/mabt/shcollection/master/varnish/default.666.vcl  --output /etc/varnish/default.$NAME.vcl
-curl https://raw.githubusercontent.com/mabt/shcollection/master/varnish/apache2-vhost-varnished-exemple.conf --output /etc/apache2/varnish-example.conf
+curl https://raw.githubusercontent.com/mabt/shcollection/master/varnish/apache2-vhost-varnished-example.conf --output /etc/apache2/varnish-example-$NAME.conf
 curl https://raw.githubusercontent.com/mabt/shcollection/master/varnish/apache2-varnish.conf --output /etc/apache2/conf-enabled/varnish.conf
 
 sed -i 's/666/'${NAME}'/g' /lib/systemd/system/varnishncsa-$NAME.service
 sed -i 's/666/'${NAME}'/g' /lib/systemd/system/varnish-$NAME.service
 sed -i 's/55555/'${PORT}'/g' /lib/systemd/system/varnish-$NAME.service
-sed -i 's/77777/'${PORT}'/g' /lib/systemd/system/varnish-$NAME.service
+sed -i 's/77777/'${PORT2}'/g' /lib/systemd/system/varnish-$NAME.service
 
 [[ -e /var/lib/varnish/${NAME} ]] || mkdir /var/lib/varnish/$NAME
 chown varnish. /var/lib/varnish/$NAME
